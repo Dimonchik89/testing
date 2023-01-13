7ec6dcb7238a2fb70a57ff458a975304c06beeba
Dimonchik89 <dima.music1989@gmail.com>
2023-01-13 14:00:02 +0200

    testing by Snapshot

--- __tests__/fetch-greeting.test.js
+++ __tests__/fetch-greeting.test.js
@@ -19,15 +19,20 @@ afterAll(() => server.close())
 
 describe("Home", () => {
     test("успешное получение приветствия", async () => {
-        render(<FetchGreeting url='/greeting'/>)
+        // render(<FetchGreeting url='/greeting'/>)
+        
+        const { container, getByDataCy } = render(<FetchGreeting url='/greeting' />)
+        // тестируем текущее состояние `UI` с помощью снимка
+        expect(container).toMatchSnapshot()
 
         fireEvent.click(screen.getByText('Получить приветствие'))
         await waitFor(() => screen.getByRole("heading"))
-        const { getByDataCy } = render(<FetchGreeting url='/greeting' />)
+        // состояние `UI` изменилось, поэтому нужен еще один снимок
+        expect(container).toMatchSnapshot()
 
         expect(getByDataCy('heading')).toHaveTextContent('Привет!')
-        expect(screen.getAllByRole("button")[0]).toHaveTextContent("Готово")
-        expect(screen.getAllByRole("button")[0]).toBeDisabled()
+        expect(screen.getByRole("button")).toHaveTextContent("Готово")
+        expect(screen.getByRole("button")).toBeDisabled()
         // expect(screen.getByRole("heading")).toHaveTextContent('Привет!')
         // expect(screen.getByRole("button")).toHaveTextContent("Готово")
         // expect(screen.getByRole("button")).toBeDisabled()
@@ -35,14 +40,18 @@ describe("Home", () => {
 
     test("обработка ошибки сервера", async () => {
         server.use(rest.get('/greeting', (req, res, cts) => res(cts.status(500))))
-        render(<FetchGreeting url="greeting"/>)
+        // render(<FetchGreeting url="greeting"/>)
+        const { container } = render(<FetchGreeting url="/greeting"/>)
+
+        expect(container).toMatchSnapshot()
 
         // const user = userEvent.setup()
         await userEvent.click(screen.getByRole("button"))
         await waitFor(() => screen.getByRole("alert"))
 
-        expect(screen.getByRole("alert")).toHaveTextContent('Не удалось получить приветствие')
+        expect(container).toMatchSnapshot()
 
+        expect(screen.getByRole("alert")).toHaveTextContent('Не удалось получить приветствие')
         expect(screen.getByRole("button")).not.toBeDisabled()
     })
 })
